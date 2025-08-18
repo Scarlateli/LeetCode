e3c3f4ca1e1a64bfded01b30a0c42532b570e47b
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-08-18 17:15:09 -0300

    Time: 846 ms (73.9%), Space: 0B (100%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+/* tabela --> Employee */
+/* tabela --> Bonus */
+
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+    ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
+
