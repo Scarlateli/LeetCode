a789d52c05722fc972821257e84b16ece5e2e48e
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-08-19 15:39:47 -0300

    Time: 830 ms (8.23%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,5 +1,6 @@
 /* Write your PL/SQL query statement below */
-select name
-from Customer
-where referee_id != 2 or referee_id is NULL
+SELECT name
+FROM Customer
+WHERE referee_id NOT IN (2) OR referee_id IS NULL;
+
 
