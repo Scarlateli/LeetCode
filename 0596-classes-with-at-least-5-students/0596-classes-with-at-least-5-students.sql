1b6319fdd26c4836f0ba8d510878c1b1f4439e6a
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-08-24 14:29:53 -0300

    Time: 398 ms (49.25%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,5 +1,4 @@
-/* Write your PL/SQL query statement below */
-select class
-from courses
-group by class
-having count (class) >=5
+SELECT class
+FROM courses
+GROUP BY class
+HAVING COUNT(*) >= 5;
