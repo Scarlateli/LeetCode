ddefa836142105697ae0b097fb635126eea1684f
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-09-02 23:56:45 -0300

    Time: 455 ms (72.76%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,5 +1,9 @@
-/* Write your PL/SQL query statement below */
-select distinct author_id as id
-from views
-where author_id = viewer_id
-order by id asc
+SELECT author_id AS id
+FROM Views v1
+WHERE EXISTS (
+    SELECT 1 FROM Views v2 
+    WHERE v2.author_id = v1.author_id 
+    AND v2.author_id = v2.viewer_id
+)
+GROUP BY author_id
+ORDER BY id ASC;
