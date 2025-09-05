5d7cb461953eea0b856ec4d21521ccedb186bb3f
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-09-04 23:46:07 -0300

    Time: 356 ms (76.36%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,5 +1,5 @@
 /* Write your PL/SQL query statement below */
-/*SELECT 
+ SELECT 
     machine_id,
     ROUND(
         (SUM(CASE WHEN activity_type = 'end' THEN timestamp ELSE 0 END) - 
@@ -8,15 +8,4 @@
         3
     ) AS processing_time
 FROM Activity
-GROUP BY machine_id;*/
-
-SELECT 
-    s.machine_id,
-    ROUND(AVG(e.timestamp - s.timestamp), 3) AS processing_time
-FROM Activity s
-JOIN Activity e ON s.machine_id = e.machine_id 
-                AND s.process_id = e.process_id
-WHERE s.activity_type = 'start' 
-  AND e.activity_type = 'end'
-GROUP BY s.machine_id
-ORDER BY s.machine_id;
+GROUP BY machine_id;
