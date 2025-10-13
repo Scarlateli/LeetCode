50115fc3a4bcd07f0c2bcc6724d54431385f3b6b
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-10-13 14:41:23 -0300

    Time: 532 ms (61.61%), Space: 0B (100%) - LeetHub

---
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -0,0 +1,18 @@
+-- todos os produtos tem valor 10
+-- achar os valores de todos os produtos na data 2019-08-16
+-- orderar em qualquer ordem 
+SELECT product_id
+     , CASE WHEN '2019-08-16' < start_dt THEN 10
+            ELSE new_price
+        END AS price
+  FROM (SELECT product_id
+             , new_price
+             , change_date AS start_dt
+             , lead(change_date) OVER (PARTITION BY product_id ORDER BY change_date) AS end_dt
+             , min(change_date) OVER (PARTITION BY product_id) AS min_dt
+          FROM products) p
+ WHERE ('2019-08-16' >= start_dt
+        AND ('2019-08-16' < end_dt
+             OR end_dt IS NULL))
+    OR ('2019-08-16' < start_dt
+        AND start_dt = min_dt);
