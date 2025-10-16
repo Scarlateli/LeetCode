613d2b1896a1b08b46f8238041cca20e249d223b
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-10-16 19:11:54 -0300

    Time: 91 ms (71.97%), Space: 38.6 MB (42.86%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
@@ -4,12 +4,10 @@ class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
         restos = Counter(num % value for num in nums)
         
-        mex = 0
-        while mex < len(nums):
-            if restos[mex % value] > 0:
-                restos[mex % value] -= 1
-                mex += 1
-            else:
-                break
+        # Encontra o resto que aparece menos vezes
+        min_count = min(restos.get(i, 0) for i in range(value))
         
-        return mex
+        # Depois das rodadas completas, qual resto falta?
+        for i in range(value):
+            if restos.get(i, 0) == min_count:
+                return min_count * value + i
