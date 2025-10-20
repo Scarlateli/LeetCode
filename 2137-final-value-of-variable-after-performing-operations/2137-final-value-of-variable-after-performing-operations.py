1a3b28015f20cf915735b104340c6f645b439dbb
Joao Pedro Scarlateli <101196488+Scarlateli@users.noreply.github.com>
2025-10-19 22:19:57 -0300

    Time: 0 ms (100%), Space: 17.6 MB (98.35%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -2,8 +2,10 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
             x = 0
             for o in operations:
-                if '+' in o: x += 1
-                else: x-= 1
+                if '+' in o: 
+                    x += 1
+                else: 
+                    x-= 1
 
             return x    
             
